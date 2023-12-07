4d0a6d95b20efe82095e85cdf1e3ab6390ae9883
Steve Meyer <stephen.meyer@wisc.edu>
2023-12-07 15:23:33 -0600

    Add CGD placeholder to output

--- transformer.rb
+++ transformer.rb
@@ -24,6 +24,7 @@ class Transformer
         document.holdings do
           record.fields(@configuration[:locations][:field]).each do |location|
             holding_statements = record.fields(@configuration[:summary_statements][:field]).select {|s| s["8"] == location["8"]}
+
             document.holding do |holding|
               holding.owningInstitutionHoldingsId(location["8"])
               holding.content do
@@ -57,16 +58,27 @@ class Transformer
 
                           # Required fields: must include and use a blank value
                           marc.subfield(item[item_config[:pid_sf]] ? item[item_config[:pid_sf]] : "", code: "a")
-                          # Use Restrictions: Field must be present. Defaulting to blank value, which implies no restrictions
+                          # Use Restrictions: Field must be present. Defaulting to blank value, which implies no restrictions.
+                          # TODO: how will the BTAA determine use restrictions?
                           marc.subfield("", code: "h")
                           marc.subfield(item[item_config[:status_sf]] ? item[item_config[:status_sf]] : "", code: "j")
                           marc.subfield(location["b"], code: "k")
                           marc.subfield(item[item_config[:barcode_sf]] ? item[item_config[:barcode_sf]] : "", code: "p")
+                          # TODO: what are the BTAA values for this code?
                           marc.subfield("IMS_LOCATION_CODE", code: "l")
+
                           # Optional fields: do not require a blank value
-                          marc.subfield(item[item_config[:copy_id_sf]], code: "t") if item[item_config[:copy_id_sf]]
+                          marc.subfield(item[item_config[:copy_id_sf]], code: "t")     if item[item_config[:copy_id_sf]]
                           marc.subfield(item[item_config[:description_sf]], code: "3") if item[item_config[:description_sf]]
                         end # bibRecord/holdings/holding/content/items/content/collection/record/datafield[tag="876"]
+
+                        marc.datafield(tag: "900", ind1: " ", ind2: " ") do
+                          # TODO: need a way to determine whether to use values Open, Shared or Private
+                          # for the Collection Group Designation (CGD)
+                          marc.subfield("Shared", code: "a")
+                          # TODO: what are the BTAA values for this code?
+                          marc.subfield("CUSTOMER_CODE", code: "b")
+                        end # bibRecord/holdings/holding/content/items/content/collection/record/datafield[tag="900"]
                       end # bibRecord/holdings/holding/content/items/content/collection/record
                     end # holding_items.each
                   end # bibRecord/holdings/holding/content/items/content/collection
